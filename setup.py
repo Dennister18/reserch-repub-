09aa13c16fbe2725310cac231493e1c32c3d56e7
Dennister18 <72225154+Dennister18@users.noreply.github.com>
2026-07-22 21:32:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="osint-research",
-    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.31.0",
